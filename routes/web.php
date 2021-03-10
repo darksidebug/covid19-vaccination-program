0f916d8fc04ef83c9cedca9f855b68166548e841
miyunecadz <jvcadz@gmail.com>
2021-03-10 16:22:54 +0800

    QR CODE must be unique (fixed)

--- routes/web.php
+++ routes/web.php
@@ -49,11 +49,8 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/logout', [UserController::class, 'logout'])->name('logout.user');
 
     Route::post('/register-user', [UserController::class, 'registerUser']);
-<<<<<<< HEAD
     Route::get('/counseling',[CounselingController::class,'index'])->name('counseling');
-=======
     Route::get('/vaccine', [VaccineController::class, 'index']);
->>>>>>> 426cd263c1b8af720765be1045ffa80c59d272f9
 });
 
 Route::get('/login', [UserController::class, 'index'])->name('login');
---
+++ tests/Feature/PeopleTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Person;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PeopleTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_can_people_register_qrcode_that_has_unique_qr_code_will_register_successfuly()
+    {
+        $response = $this->post('/api/register-person', [
+            'qr_code' => '07292020-1379',
+            'first_name' => 'June Vic',
+            'middle_name' => 'Wales',
+            'last_name' => 'Cadayona',
+            'province' => 'Southern Leyte',
+            'municipality' => 'Tomas Oppus',
+            'barangay' => 'Bogo (Pob.)',
+            'purok' => 'sample',
+            'contact' => '09178781045',
+            'birth_date' => '2020-2-5',
+            'gender' => 'Male',
+            'civil_status' => 'Single',
+            'category' => 'sadasd',
+            'category_id' => 'dsadsa',
+            'category_id_number' => 'dsadsa',
+            'employment_status' => 'dsadsa',
+            'direct_contact' => 'dsadsa',
+            'profession' => 'sadsadsa',
+            'pregnant_status' => 'False',
+            'name_employer' => 'sads',
+            'province_employer' => 'dsa',
+            'municipality_employer' => 'dsa',
+            'barangay_employer' => 'dsa',
+            'contact_number_employer' => 'dsa',
+            'comorbidity' => 'None',
+            'comorbidity_yes' => 'rdsadsad',
+            'allergy' => 'none',
+            'allergy_yes' => 'dsadsa',
+            'diagnose_covid' => 'none',
+            'date_diagnose_covid_yes' => '',
+            'covid_classification' => '',
+            'electronic_informed_consent' => 'None'
+        ]);
+
+        $response->assertJson(['status' => 'success']);
+    }
+
+    public function test_can_people_register_qrcode_that_has_not_unique_qr_code_will_fail()
+    {
+
+        $person=Person::factory()->create([
+            'qr_code'=>'07292020-1379'
+        ]);
+
+        $response = $this->post('/api/register-person', [
+            'qr_code' => '07292020-1379',
+            'first_name' => 'June Vic',
+            'middle_name' => 'Wales',
+            'last_name' => 'Cadayona',
+            'province' => 'Southern Leyte',
+            'municipality' => 'Tomas Oppus',
+            'barangay' => 'Bogo (Pob.)',
+            'purok' => 'sample',
+            'contact' => '09178781045',
+            'birth_date' => '2020-2-5',
+            'gender' => 'Male',
+            'civil_status' => 'Single',
+            'category' => 'sadasd',
+            'category_id' => 'dsadsa',
+            'category_id_number' => 'dsadsa',
+            'employment_status' => 'dsadsa',
+            'direct_contact' => 'dsadsa',
+            'profession' => 'sadsadsa',
+            'pregnant_status' => 'False',
+            'name_employer' => 'sads',
+            'province_employer' => 'dsa',
+            'municipality_employer' => 'dsa',
+            'barangay_employer' => 'dsa',
+            'contact_number_employer' => 'dsa',
+            'comorbidity' => 'None',
+            'comorbidity_yes' => 'rdsadsad',
+            'allergy' => 'none',
+            'allergy_yes' => 'dsadsa',
+            'diagnose_covid' => 'none',
+            'date_diagnose_covid_yes' => '',
+            'covid_classification' => '',
+            'electronic_informed_consent' => 'None'
+        ]);
+
+        $response->assertJson(['status' => 'error']);
+    }
+}
